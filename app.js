33c7c34153f42924fb3a953d10c25107b1feee0d
ayush kumar singh <singhayushkumar135@gmail.com>
2026-06-09 12:32:47 +0530

    add vercel config

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ config({ path: "./config.env" });
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
